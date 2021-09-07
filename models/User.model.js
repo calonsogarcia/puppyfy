63521b088497320eadbb05e72f0038b531c98cea
lucia vilar <vp_lucia@hotmail.com>
2021-09-07 21:33:37 +0200

    begining

--- models/User.model.js
+++ models/User.model.js
@@ -2,11 +2,28 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  username: {
-    type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
-  },
-  password: String,
+    username: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+        unique: true,
+    },
+    fullName: String,
+    dateOfBirth: Number,
+    sex: String,
+    address: String,
+    phone: Number,
+    job: String,
+    familyStructure: {
+        numberOfMembers: Number,
+        children: Number,
+        otherPets: Boolean,
+    },
+
+    comments: String,
+
 });
 
 const User = model("User", userSchema);
