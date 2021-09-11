5e977cf24915133bcf3e4a45e7e3ba24e1b6af0d
calonsogarcia <carolina.alon.garcia@gmail.com>
2021-09-11 11:13:05 +0200

    trying to save

--- models/User.model.js
+++ models/User.model.js
@@ -15,7 +15,7 @@ const userSchema = new Schema({
     email: {
         type: String,
         unique: true,
-        required: true
+        required: true,
     },
     dateOfBirth: Number,
     sex: String,
