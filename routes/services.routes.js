4975df1c3ca3d1378745aa63664cbfb0bb93d829
calonsogarcia <carolina.alon.garcia@gmail.com>
2021-09-14 16:23:14 +0200

    Services views and routes created

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -4,9 +4,25 @@ const ServicesModel = require('../models/Services.model')
 router.get("/", (req, res, next) => {
     res.render("services/list.hbs");
 });
+
+router.get("/hairdressers", (req, res, next) => {
+    res.render("services/hairdressers.hbs");
+});
+
+router.get("/puppy-sitters", (req, res, next) => {
+    res.render("services/puppy-sitters.hbs");
+});
+
+router.get("/puppy-trainers", (req, res, next) => {
+    res.render("services/puppy-trainers.hbs");
+});
+
+
+
 router.get("/partners-form", (req, res, next) => {
     res.render("services/partners-form.hbs");
 })
+
 router.post("/partners-form", (req, res, next) => {
     const { name, adress, contact } = req.body
     ServicesModel.create({ name, adress, contact })
